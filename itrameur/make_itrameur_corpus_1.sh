f75666594567ece1a58b23688509f0fa14ddd273
mariahul1 <hul.maria88@gmail.com>
2024-01-06 16:29:18 +0100

    update pol itrameur

--- itrameur/make_itrameur_corpus_1.sh
+++ itrameur/make_itrameur_corpus_1.sh
@@ -9,31 +9,28 @@ DOSSIER_DUMPS=$1
 DOSSIER_CONTEXTES=$2
 LANG=$3
 
-output_dump="../PPE_lien/itrameur/dumps-text_${LANG}.txt"
+output_dump="./itrameur/dump_${LANG}.txt"
 echo "<LANG=\"${LANG}\">" > "$output_dump"
 
-output_contextes="../PPE_lien/itrameur/contextes_${LANG}.txt"
+output_contextes="./itrameur/contexte_${LANG}.txt"
 echo "<LANG=\"${LANG}\">" > "$output_contextes"
 
-for FICHIER in "$DOSSIER_DUMPS"/dump_"${LANG}"*.html; 
+for FICHIER in "$DOSSIER_DUMPS"/dump_${LANG}*[.html.txt]; 
 do
     if [ -f "$FICHIER" ]; then
 		page=$(basename $FICHIER| sed "s/dump_//;s/\.html$//")
-        contenu=$(cat $FICHIER | tr -d '&<>' | sed -e 's/&/&amp;/g' -e 's/</&lt;/g' -e 's/>/&gt;/g')
+        contenu=$(cat "$FICHIER" | tr -d '&<>' | sed -e 's/&/&amp;/g' -e 's/</&lt;/g' -e 's/>/&gt;/g')
         echo "<page=\"${page}\">" >> "$output_dump"
         echo "<text>${contenu}</text>" >> "$output_dump"
         echo "</page> §" >> "$output_dump"
-        #cleaned = $( echo $contenu | sed 's/&/&amp;/g')
-        #cleaned = $( echo $contenu | sed 's/</&lt;/g')
-        #cleaned = $( echo $contenu | sed 's/>/&gt;/g')
     fi
 done
 echo "</LANG>" >> "$output_dump"
 
-for FICHIER in "$DOSSIER_CONTEXTES"/contexte_"${LANG}"*.txt; 
+for FICHIER in "$DOSSIER_CONTEXTES"/contexte_${LANG}*.txt; 
 do
     if [ -f "$FICHIER" ]; then
-		page=$(basename $FICHIER| sed "s/contexte_//;s/\.txt$//")
+        page=$(basename "$FICHIER" | sed "s/contexte_//;s/\.txt$//")
         contenu=$(cat "$FICHIER" | tr -d '&<>' | sed -e 's/&/&amp;/g' -e 's/</&lt;/g' -e 's/>/&gt;/g')
         echo "<page=\"${page}\">" >> "$output_contextes"
         echo "<text>${contenu}</text>" >> "$output_contextes"
