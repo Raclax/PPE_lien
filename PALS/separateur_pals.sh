f6e17ce82b4210e38ebee365001ed0fcc6bf2ebe
mariahul1 <hul.maria88@gmail.com>
2024-01-07 22:16:51 +0100

    modif

--- PALS/separateur_pals.sh
+++ PALS/separateur_pals.sh
@@ -21,26 +21,4 @@ echo "Mots séparés avec succès. Le résultat a été enregistré dans $fichie
 # python3 ./programmes/korean.py ./PALS/dump_pals_kor.txt   --target "관계" --match-mode regex -N 10 --tool-emulation itrameur > ./PALS/resultats_pals_kor.txt
 
 
-# #!/usr/bin/env bash
-
-# FOLDER=$1
-# URL=$2
-# URL_FILE="../urls/$URL.txt"
-
-# if [ $# -ne 2 ] # verifier si le script a un argument
-# then
-# 	echo "ce programme demande deux arguments: un argument pour le chemin vers le dossier itrameur et le fichier text format ro_ukraine, ro_israel, ro_general" 
-# 	exit
-# fi
-
-# N=1
-# #supprimer les fichier avant de commencer, pour ne pas avoir les memes fichier plusieurs fois si on execute le script de nouveau
-# echo "" > $FOLDER/dump-pals-$URL.txt
-# echo "" > $FOLDER/contexte-pals-$URL.txt
-
-# while read -r line;
-# do
-# 	cat ../dumps-text/dump_${URL}_$N.txt | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" >> $FOLDER/dump-pals-$URL.txt
-# 	cat ../contexte/contexte_${URL}_$N.txt | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" >> $FOLDER/contexte-pals-$URL.txt
-# 	N=$(expr $N + 1)
-# done < $URL_FILE
+#
