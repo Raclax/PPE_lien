f0ae4917717055a272566e639c9b610eef2d4180
mariahul1 <hul.maria88@gmail.com>
2024-01-07 11:59:37 +0100

    update pals

---
+++ PALS/separateur_pals.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+  echo "Utilisation : $0 fichier_input fichier_output"
+  exit 1
+fi
+
+fichier_input=$1
+fichier_output=$2
+
+# Vérifier si le fichier d'entrée existe
+if [ ! -f "$fichier_input" ]; then
+  echo "Erreur : fichier d'entrée n'existe pas"
+  exit 1
+fi
+
+cat "$fichier_input" | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g"  > "$fichier_output"
+
+echo "Mots séparés avec succès. Le résultat a été enregistré dans $fichier_output"
+
+
+
+
+# #!/usr/bin/env bash
+
+# FOLDER=$1
+# URL=$2
+# URL_FILE="../urls/$URL.txt"
+
+# if [ $# -ne 2 ] # verifier si le script a un argument
+# then
+# 	echo "ce programme demande deux arguments: un argument pour le chemin vers le dossier itrameur et le fichier text format ro_ukraine, ro_israel, ro_general" 
+# 	exit
+# fi
+
+# N=1
+# #supprimer les fichier avant de commencer, pour ne pas avoir les memes fichier plusieurs fois si on execute le script de nouveau
+# echo "" > $FOLDER/dump-pals-$URL.txt
+# echo "" > $FOLDER/contexte-pals-$URL.txt
+
+# while read -r line;
+# do
+# 	cat ../dumps-text/dump_${URL}_$N.txt | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" >> $FOLDER/dump-pals-$URL.txt
+# 	cat ../contexte/contexte_${URL}_$N.txt | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" >> $FOLDER/contexte-pals-$URL.txt
+# 	N=$(expr $N + 1)
+# done < $URL_FILE
